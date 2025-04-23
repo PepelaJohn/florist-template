63f57eefa70a69d9a59cf30c11d2c8fe95f42d62
Pepela John <pepelajahy@gmail.com>
2025-04-23 17:23:56 +0300

    florist

--- src/components/sections/Subscriptions.tsx
+++ src/components/sections/Subscriptions.tsx
@@ -88,7 +88,7 @@ export default function Subscriptions() {
             />
             <div className="absolute inset-0 bg-black/20"></div>
             <div className="absolute inset-0 flex items-center justify-center">
-              <h3 className="text-white text-2xl font-light uppercase tracking-wider">New Season Tulips</h3>
+              <h3 className="text-white text-2xl font-light uppercase tracking-wider grid-col-1">New Season Tulips</h3>
             </div>
           </div>
         </div>
